2c51aee7ba3aa55030acfb406ad71285a203c2e0
dhairya <dhairyasenjaliya@gmail.com>
2019-02-13 14:56:36 +0530

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <input type="text">
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+
+</script>
+
+ 
